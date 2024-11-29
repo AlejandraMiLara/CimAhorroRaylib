60b768fae608d655ea51fcab3cf08ec036f50052
AlejandraMiLara <a365544@uabc.edu.mx>
2024-11-28 21:22:08 -0800

    cambie el setup

--- main.c
+++ main.c
@@ -1,55 +1,60 @@
 #include "raylib.h"
 
 typedef enum GameScreen {
-     MENU, 
-     AHORROS,
-     AHORRAR,
+    SCREEN_MENU, 
+    SCREEN_AHORROS,
+    SCREEN_MAX,
 } GameScreen;
+
+// Definir la variable global currentScreen
+GameScreen currentScreen = SCREEN_MENU;
+
+void actualizarScreens() {
+    if (IsKeyPressed(KEY_RIGHT)) {
+        currentScreen = (currentScreen + 1) % SCREEN_MAX;
+    }
+
+    if (IsKeyPressed(KEY_LEFT)) {
+        currentScreen = (currentScreen - 1 + SCREEN_MAX) % SCREEN_MAX;
+    }
+}
+
+void dibujarScreens() {
+    ClearBackground(RAYWHITE);
+
+    switch (currentScreen) {
+        case SCREEN_MENU:
+            DrawText("MENU", 350, 200, 20, BLACK);
+            break;
+        case SCREEN_AHORROS:
+            DrawText("MIS AHORROS", 350, 200, 20, BLACK);
+            break;
+        default:
+            break;
+    }
+}
 
 int main(void)
 {
-    //CONFIGURACIONES INICIALES
+    // CONFIGURACIONES INICIALES
     const int screenWidth = 1920;
     const int screenHeight = 1080;
     SetConfigFlags(FLAG_MSAA_4X_HINT);
     InitAudioDevice();
     InitWindow(screenWidth, screenHeight, "CIMAHORRO");
-    GameScreen currentScreen = MENU;
     SetTargetFPS(60);
 
-    //VARIABLES DE ENTORNO
-
-
-    //BUCLE DE LÓGICA
-    while(!WindowShouldClose())
-    {
-        switch (currentScreen)
-        {
-            case MENU:
-                break;
-            
-            default:
-                break;
-        }
-    }
- 
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-
-    //BUCLE DE DIBUJO
-    while (!WindowShouldClose())
-    {
-        switch (currentScreen)
-        {
-        case MENU:
-            break;
+    // BUCLE PRINCIPAL
+    while (!WindowShouldClose()) {
+        // LÓGICA
+        actualizarScreens();
         
-        default:
-            break;
-        }
+        // DIBUJO
+        BeginDrawing();
+        dibujarScreens();
         EndDrawing();
     }
-
+ 
     CloseWindow();
     
     return 0;
